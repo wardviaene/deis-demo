b237198ceba329e0e44165f3d7f6359d488c8364
Edward Viaene <ward.viaene@bigdatapartnership.com>
2016-05-28 19:38:18 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,3 +1,9 @@
 variable "DO_TOKEN" {}
-variable "PUBLIC_SSH_KEY" {}
-variable "PRIVATE_SSH_KEY" {}
+
+variable "PUBLIC_SSH_KEY" {
+  default = "~/.ssh/mykey.pub"
+}
+
+variable "PRIVATE_SSH_KEY" {
+  default = "~/.ssh/mykey"
+}
